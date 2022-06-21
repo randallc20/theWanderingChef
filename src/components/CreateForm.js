3d7b30aa37f91441f101ca78baf0da2e3b0ef115
chrisrandall <chris.f.randall@gmail.com>
2022-06-21 16:01:06 -0600

    create form

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,15 +1,104 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import './CreateForm.css'
 
 function CreateForm() {
+  const [description, setDescription] = useState("")
+  const [name, setName] = useState("")
+  const [image, setImage] = useState("")
+  const [ingredients, setIngredients] = useState("")
+  const [season, setSeason] = useState("")
+  const [category, setCategory] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch('http://localhost:3001/recipes', {
+      method:'POST',
+      headers: {'Content-type': 'application/json'}, 
+      body: JSON.stringify({
+        description: description,
+        name: name,
+        image: image,
+        ingredients: ingredients,
+        season: season,
+        category: category,
+      }),
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    //setName("")
+  }
+
   return (
-    <div>
-      <form id="new-recipe-form">
-        <input type="text" name="name" placeholder="Recipe Name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <button type="submit">Add Recipe</button>
+    <div className="container">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <h1>Create Recipe</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder='Add recipe name...'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label>Description</label>
+            <input
+              type="text"
+              id="description"
+              name="description"
+              placeholder='Add item description...'
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label>Ingredients</label>
+            <input
+              type="text"
+              id="ingredients"
+              name="ingredients"
+              placeholder='Add ingredients...'
+              onChange={(e) => setIngredients(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label>Image</label>
+            <input
+              type="text"
+              id="image"
+              name="image"
+              placeholder='Add item image link'
+              onChange={(e) => setImage(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label>Season</label>
+            <input
+              type="text"
+              id="season"
+              name="season"
+              placeholder='Add season'
+              onChange={(e) => setSeason(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label>Category</label>
+            <input
+              type="text"
+              id="category"
+              name="category"
+              placeholder='Add category'
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </div>
+          <button>Submit</button>
+        </div>
       </form>
-        <textarea type="text" name="Instructions" form="new-recipe-form" placeholder="Enter instructions:"></textarea>
     </div>
+    
   );
 }
 
